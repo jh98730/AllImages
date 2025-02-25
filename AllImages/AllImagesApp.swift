f19706a0e1152f17497b763d5cfdf4495d99668e
John Howard <john.k.howard@gmail.com>
2025-02-25 17:14:45 -0500

    Initial Commit

---
+++ AllImages/AllImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllImagesApp.swift
+//  AllImages
+//
+//  Created by John Howard on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AllImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
